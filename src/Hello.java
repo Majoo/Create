fb3af82a481b953cf546098ceb89b31586baa9fb
Patricia <chorriballong@gmail.com>
2013-03-19 14:09:09 +0100

    my first commit *hihi*

--- src/Hello.java
+++ src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hello World!");
+		JOptionPane.showMessageDialog(null, "Hello Nerd!");
 
 	}
 

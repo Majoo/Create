099db58c087e0b00b3bb94c743af88767e8756ad
Patricia <chorriballong@gmail.com>
2013-03-19 14:12:24 +0100

    changes

--- src/Hello.java
+++ src/Hello.java
@@ -5,10 +5,10 @@ public class Hello {
 
 	/**
 	 * @param args
+	 * @wbp.parser.entryPoint
 	 */
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Hello HONEYS!");
-
 	}
 
 }

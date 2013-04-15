3abe8b9015a513c4bb472b4db60f353dcff2765b
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-04-15 19:00:04 +0200

    Observation made in Document class
    Assignment of variables was missing, and to avoid  duplicaation of code,
    a suggestion has been put forward.

--- src/se/chalmers/tda367/group25/resumate/Document.java
+++ src/se/chalmers/tda367/group25/resumate/Document.java
@@ -4,23 +4,34 @@ import java.awt.Image;
 
 import se.chalmers.tda367.group25.resumate.utils.TemplateStorage;
 
-
-
-
 public class Document {
 
 	private IOHandler ioH;
 	private Template templ;
 	private History history;
 
-	public Document(){
+	public Document() {
 		templ = TemplateStorage.getTemplate("defaultCV");
 		history = new History();
 		ioH = new IOHandler();
 	}
 
-	public Document(Template templ){
+	public Document(Template templ) {
 		this.templ = templ;
 	}
 
+	/**
+	 * At the moment, we're lacking assignments of templ and history in the
+	 * second constructor. Should we solve this by doing the following:
+	 * 
+	 * public Document() {
+	 * 		this(TemplateStorage.getTemplate("defaultCV"));
+	 * }
+	 * 
+	 * public Document(Template templ){
+	 * 		this.templ = templ;
+	 * 		history = new History();
+	 * 		ioH = new IOHandler();
+	 * }
+	 */
 }

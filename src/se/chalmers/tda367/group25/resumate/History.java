375c5a2768ca7a8a8bd2b07fe40cb303662b0990
Patricia <chorriballong@gmail.com>
2013-04-16 16:34:17 +0200

    ok I'm done for todayz

--- src/se/chalmers/tda367/group25/resumate/History.java
+++ src/se/chalmers/tda367/group25/resumate/History.java
@@ -1,5 +1,31 @@
 package se.chalmers.tda367.group25.resumate;
 
-public class History {
+import java.util.List;
 
+public class History {
+	//The list could have versions of the document
+	//stored. Ideas not finished.
+	private List<Document> versions;
+	
+	
+	/*
+	 * Adds a version to the list. 
+	 * 
+	 * There should be a check so that the list only 
+	 * contains "so many" versions i.e. it should be 
+	 * cleaned once in a while.
+	 */
+	public void addVersion(){
+		
+	}
+	
+	/*
+	 * Get the previous version of the document.
+	 * List should be updated accordingly ;)
+	 */
+
+	public Document getPrevious(){
+		return null;
+	}
+	
 }
--- src/se/chalmers/tda367/group25/resumate/ResuMate.java
+++ src/se/chalmers/tda367/group25/resumate/ResuMate.java
@@ -8,22 +8,31 @@ import se.chalmers.tda367.group25.resumate.utils.Template;
 
 
 public class ResuMate {
+	
+	private User user;
 	private List<Document> docs = new ArrayList<Document>();
 
-
+	/*An application with the default document is created.*/
 	public ResuMate(){
 		Document doc = new Document();
 		docs.add(doc);
 	}
 
+	/*Creates a new document*/
 	public void createDocument(Template templ){
 		Document doc = new Document(templ);
 		docs.add(doc);
 
 	}
 
+	/*Get the document of index i*/
 	public Document getDoc(int i){
 		return docs.get(i);
 
+	}
+	
+	/*Quit. Documents should be saved*/
+	public void quit(){
+		
 	}
 }

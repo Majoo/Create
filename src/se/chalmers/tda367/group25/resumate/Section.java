5aaccd41404b0495035a23340280ccc1d7d6cba9
Patricia <chorriballong@gmail.com>
2013-04-16 15:14:47 +0200

    Section only stores SectionNames

--- src/se/chalmers/tda367/group25/resumate/Section.java
+++ src/se/chalmers/tda367/group25/resumate/Section.java
@@ -1,7 +1,23 @@
 package se.chalmers.tda367.group25.resumate;
 
-public abstract class Section {
+import se.chalmers.tda367.group25.resumate.utils.SectionName;
 
-	//Will include tips
+public abstract class Section {
+	
+	private SectionName secName;
+	
+	//Constructors
+	public Section(){
+		this.secName = SectionName.EMPTY;
+	}
+	
+	public Section(SectionName secName){
+		this.secName = secName;
+	}
+	
+	//Getters
+	public SectionName getSecName(){
+		return secName;
+	}
 	
 }

12469effcd4d2f2db36472a7e0fe08af3f2eaaea
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-04-21 14:26:57 +0200

    JavaDoc and outlines RMImage and RMText
    Added JavaDoc where comments where missing, remade where they were
    already present. Made outlines to some missing methods.

--- src/se/chalmers/tda367/group25/resumate/model/RMImage.java
+++ src/se/chalmers/tda367/group25/resumate/model/RMImage.java
@@ -2,33 +2,68 @@ package se.chalmers.tda367.group25.resumate.model;
 
 import java.awt.Image;
 
+public class RMImage {
 
-
-public class RMImage{
-	
 	private Image image;
 	private SectionType secType;
 
-	public RMImage(Image image){
+	public RMImage(Image image) {
 		this.secType = SectionType.IMAGE;
 		this.image = image;
 	}
 
-	//-----Getters-----//
+	/**
+	 * Crop the Image of this RMImage.
+	 * 
+	 * @param x
+	 *            pixels to crop horizontally
+	 * @param y
+	 *            pixels to crop vertically
+	 */
+	public void cropImage(int x, int y) {
+
+	}
+
+	/**
+	 * Resize the Image of this RMImage.
+	 * 
+	 * @param x
+	 *            percentage to resize horizontally
+	 * @param y
+	 *            percentage to resize vertically
+	 */
+	public void resizeImage(int x, int y) {
+
+	}
+
+	/**
+	 * Reorient the Image of this RMImage. A positive parameter rotates the
+	 * image counterclockwise, and vice versa.
+	 * 
+	 * @param v
+	 *            degrees to rotate
+	 */
+	public void reorientImage(int v) {
+
+	}
+
+	// -----Getters-----//
 	/**
 	 * Get the Image.
+	 * 
 	 * @return image
 	 */
-	public Image getImage(){
+	public Image getImage() {
 		return image;
 	}
-	
-	//-----Setters-----//
+
+	// -----Setters-----//
 	/**
 	 * Set a new image.
+	 * 
 	 * @param image
 	 */
-	public void setImage(Image image){
+	public void setImage(Image image) {
 		this.image = image;
 	}
 
--- src/se/chalmers/tda367/group25/resumate/model/RMText.java
+++ src/se/chalmers/tda367/group25/resumate/model/RMText.java
@@ -2,32 +2,58 @@ package se.chalmers.tda367.group25.resumate.model;
 
 import javax.swing.JOptionPane;
 
+public class RMText {
 
-public class RMText{
-	
 	private String text;
 	private SectionType secType;
 
-	//Constructors
-	public RMText(){
+	/**
+	 * Default constructor of a RMtext Section in a Document.
+	 */
+	public RMText() {
 		this.secType = SectionType.EMPTY;
 	}
-	
-	public RMText(SectionType sectionType){
+
+	/**
+	 * Constructs a RMText with the specified SectionType in a Document.
+	 * 
+	 * @param sectionType
+	 *            the specified SectionType, deciding what kind of Section this
+	 *            RMText is
+	 */
+	public RMText(SectionType sectionType) {
 		this.secType = sectionType;
 	}
-	
-	//-----Getters-----//
-	public String getText(){
+
+	// -----Getters-----//
+
+	/**
+	 * Returns the String text from this RMText.
+	 * 
+	 * @return the String text from this RMText
+	 */
+	public String getText() {
 		return this.text;
 	}
-	
-	public SectionType getSecType(){
+
+	/**
+	 * Returns SectionType of this RMText.
+	 * 
+	 * @return the SectionType of this RMText
+	 */
+	public SectionType getSecType() {
 		return this.secType;
 	}
-	
-	//-----Setters-----//
-	public void setText(String input){
+
+	// -----Setters-----//
+
+	/**
+	 * Sets the String text to the parameter input.
+	 * 
+	 * @param input
+	 *            the new String to which to set the String text
+	 */
+	public void setText(String input) {
 		this.text = input;
 	}
 

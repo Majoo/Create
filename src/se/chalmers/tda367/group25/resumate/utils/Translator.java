ad74476f65269150e4e813241a78ea53507ef8a3
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-04-21 13:07:46 +0200

    Created and outlined Translator.

---
+++ src/se/chalmers/tda367/group25/resumate/utils/Translator.java
@@ -0,0 +1,32 @@
+package se.chalmers.tda367.group25.resumate.utils;
+
+import java.awt.Container;
+import java.awt.Panel;
+
+import se.chalmers.tda367.group25.resumate.model.SectionType;
+import se.chalmers.tda367.group25.resumate.model.Template;
+
+public class Translator {
+
+	/**
+	 * Translates a DocumentView Panel to a Template.
+	 * 
+	 * @param p
+	 *            the Panel to translate
+	 * @return the corresponding Template
+	 */
+	public static Template panelToTemplate(Panel p) {
+		return null;
+	}
+
+	/**
+	 * Translates a Container, typically a JEditorPane, to a SectionType.
+	 * 
+	 * @param c
+	 *            the Container to translate
+	 * @return the corresponding SectionType
+	 */
+	public static SectionType containerToSectionType(Container c) {
+		return null;
+	}
+}

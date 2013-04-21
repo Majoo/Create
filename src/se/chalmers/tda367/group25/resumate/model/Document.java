7b7b6c19c4fc16eaea694b6c48397ca2ca763c4e
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-04-21 14:04:02 +0200

    JavaDoc and fixed Document
    Remade the comments as JavaDoc, removed some bits that are no longer the
    responsibility of Document

--- src/se/chalmers/tda367/group25/resumate/model/Document.java
+++ src/se/chalmers/tda367/group25/resumate/model/Document.java
@@ -1,109 +1,153 @@
 package se.chalmers.tda367.group25.resumate.model;
+
 import java.awt.Image;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import se.chalmers.tda367.group25.resumate.controllers.IOController;
-
-
 public class Document {
-	//helpers
-	private IOController ioH;
-	private History history;
-	//other instance variables
+
 	private Template currentTempl;
-	//sections
-	private Map <SectionType, RMText> texts = new HashMap<SectionType, RMText>(3);
+	// Sections
+	private Map<SectionType, RMText> texts = new HashMap<SectionType, RMText>(3);
 	private RMImage rmI;
 
-	/* Create a new Document using the default template. */
-	public Document(){
+	private History history;
+
+	/**
+	 * Create a new Document using the default Template.
+	 */
+	public Document() {
 		new Document(Template.DEF_CV);
 	}
 
-	/* Create a new Document using the specified template. */
+	/**
+	 * Create a new Document using the specified Template.
+	 * 
+	 * @param templ
+	 *            the Template to which currentTempl will be set
+	 */
 	public Document(Template templ) {
-		//Set variables
+		// Set variables
 		this.currentTempl = templ;
 		history = new History();
-		ioH = new IOController();
 		rmI = new RMImage(null);
-		//create sections according to Template.
+		// create Sections according to Template.
 		createSections();
 	}
 
-	/*Create sections according to Template.*/
-	public void createSections(){
-		switch(currentTempl){
-		
+	/**
+	 * Create necessary Sections according to Template. Checks if Sections have
+	 * already been created; if they haven't, this method creates them
+	 */
+	public void createSections() {
+		switch (currentTempl) {
+
 		case DEF_CV:
-		
-			if(!texts.containsKey(SectionType.PERSONAL_INFO)){
-				texts.put(SectionType.PERSONAL_INFO, new RMText(SectionType.PERSONAL_INFO));
+
+			if (!texts.containsKey(SectionType.PERSONAL_INFO)) {
+				texts.put(SectionType.PERSONAL_INFO, new RMText(
+						SectionType.PERSONAL_INFO));
 			}
-			if(!texts.containsKey(SectionType.WORK_EXPERIENCE)){
-				texts.put(SectionType.WORK_EXPERIENCE, new RMText(SectionType.WORK_EXPERIENCE));
+			if (!texts.containsKey(SectionType.WORK_EXPERIENCE)) {
+				texts.put(SectionType.WORK_EXPERIENCE, new RMText(
+						SectionType.WORK_EXPERIENCE));
 			}
-			//texts.add(new RMText(SectionType.EMPTY));
-			
+			// texts.add(new RMText(SectionType.EMPTY));
+			break;
 		case DEF_PL:
-			if(!texts.containsKey(SectionType.PERSONAL_INFO)){
-				texts.put(SectionType.PERSONAL_INFO, new RMText(SectionType.PERSONAL_INFO));
+			if (!texts.containsKey(SectionType.PERSONAL_INFO)) {
+				texts.put(SectionType.PERSONAL_INFO, new RMText(
+						SectionType.PERSONAL_INFO));
 			}
-			//texts.add(new RMText(SectionType.EMPTY));
-			
+			// texts.add(new RMText(SectionType.EMPTY));
+			break;
 		case CLASSY_CV:
-			if(!texts.containsKey(SectionType.PERSONAL_INFO)){
-				texts.put(SectionType.PERSONAL_INFO, new RMText(SectionType.PERSONAL_INFO));
+			if (!texts.containsKey(SectionType.PERSONAL_INFO)) {
+				texts.put(SectionType.PERSONAL_INFO, new RMText(
+						SectionType.PERSONAL_INFO));
 			}
-			if(!texts.containsKey(SectionType.WORK_EXPERIENCE)){
-				texts.put(SectionType.WORK_EXPERIENCE, new RMText(SectionType.WORK_EXPERIENCE));
+			if (!texts.containsKey(SectionType.WORK_EXPERIENCE)) {
+				texts.put(SectionType.WORK_EXPERIENCE, new RMText(
+						SectionType.WORK_EXPERIENCE));
 			}
+			break;
 		}
 	}
 
-	//---Getters---//
-	
-	 /* Get the image*/
-	public RMImage getImage(){
+	// ---Getters---//
+
+	/**
+	 * Get the image in the RMImage of the Document.
+	 * 
+	 * @return the image
+	 */
+	public RMImage getImage() {
 		return rmI;
 	}
-	
-	//---Setters---//
-	
-	/*Change the image.*/
-	public void setImage(Image image){
+
+	// ---Setters---//
+
+	/**
+	 * Sets the image of the RMImage of the Document.
+	 * 
+	 * @param image
+	 *            image to be used in the document
+	 */
+	public void setImage(Image image) {
 		this.rmI.setImage(image);
 	}
 
-	/*Change the Template*/
-	public void setTemplate(Template tmpl){
+	/**
+	 * Change the Template and create Sections accordingly.
+	 * 
+	 * @param tmpl
+	 *            the Template to change to
+	 */
+	public void setTemplate(Template tmpl) {
 		this.currentTempl = tmpl;
 		createSections();
 	}
-	
+
+	/**
+	 * Get the Map of the RMTexts of the Document.
+	 * 
+	 * @return the Map of the RMTexts
+	 */
 	public Map<SectionType, RMText> getTexts() {
 		return texts;
 	}
-	
-	public void changeText (SectionType st, String text){
-	if (!texts.containsKey(SectionType.WORK_EXPERIENCE) || 
-			!texts.containsKey(SectionType.PERSONAL_INFO) ){
-				createSections();
+
+	/**
+	 * Change the content of an RMText in the Map of the RMTexts of the
+	 * Document. If the specified RMText Section doesn't exist, create it
+	 * 
+	 * @param st
+	 *            the SectionType identity of the RMText
+	 * @param text
+	 *            the text to change to
+	 */
+	public void changeText(SectionType st, String text) {
+		if (!texts.containsKey(SectionType.WORK_EXPERIENCE)
+				|| !texts.containsKey(SectionType.PERSONAL_INFO)) {
+			createSections();
 		}
 
-	texts.get(st).setText(text);
+		texts.get(st).setText(text);
 	}
 
-	public void changeTemplate(Template temp){
-	/*List <SectionType> secs = TemplateToSections.translate(temp);
-	List <String> text = new ArrayList <String>(3);
-		for(int i = 0; i < secs.size(); i++) {
-			text.add(texts.get(secs.get(i)).getText());
-		}*/
-	setTemplate(temp);
+	/**
+	 * Change the Template of the Document.
+	 * 
+	 * @param temp
+	 *            the Template to change to
+	 */
+	public void changeTemplate(Template temp) {
+		/*
+		 * List <SectionType> secs = TemplateToSections.translate(temp); List
+		 * <String> text = new ArrayList <String>(3); for(int i = 0; i <
+		 * secs.size(); i++) { text.add(texts.get(secs.get(i)).getText()); }
+		 */
+		setTemplate(temp);
 	}
-		
+
 }

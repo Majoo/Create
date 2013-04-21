b3bf6123367138222e7c8a0238586a69c5a90b71
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-04-21 12:34:14 +0200

    MainController JavaDoc added. v QUESTION inside v
    Should MainController really handle the creation of new Documents?

--- src/se/chalmers/tda367/group25/resumate/controllers/MainController.java
+++ src/se/chalmers/tda367/group25/resumate/controllers/MainController.java
@@ -1,29 +1,39 @@
 package se.chalmers.tda367.group25.resumate.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import se.chalmers.tda367.group25.resumate.model.Document;
 import se.chalmers.tda367.group25.resumate.model.Template;
+import se.chalmers.tda367.group25.resumate.views.DocumentView;
+import se.chalmers.tda367.group25.resumate.views.MainView;
 
 public class MainController {
 
 	private DocumentController documentController;
 	private IOController ioController;
+	private MainView mainView;
 
-	/*An application with the default document is created.*/
-	public MainController(){
-		documentController = new DocumentController();
+	/* An application with the default document is created. */
+	public MainController() {
+		documentController = new DocumentController(new Document(),
+				new DocumentView());
 		ioController = new IOController();
 	}
 
-	/*Creates a new document with the specified template*/
-	public void createDocument(Template templ){
-		Document doc = new Document(templ);
-	}
-	
-	/*Quit. Documents should be saved*/
-	public void quit(){
-		
+	// Isn't this unnecessary? New Documents should be created in
+	// DocumentController, or?
+	// /**
+	// * Creates a new Document with the specified Template.
+	// *
+	// * @param templ
+	// * the Template on which to base the Document
+	// */
+	// public void createDocument(Template templ) {
+	// Document doc = new Document(templ);
+	// }
+
+	/**
+	 * Quit. Documents should be saved
+	 */
+	public void quit() {
+
 	}
 }

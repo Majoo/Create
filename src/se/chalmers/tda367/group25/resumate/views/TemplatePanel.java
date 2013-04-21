a6e6e35fa8d1000bdf788a6fd5e166b476d6c626
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-04-21 13:46:53 +0200

    Created and outlined TemplatePanel.

---
+++ src/se/chalmers/tda367/group25/resumate/views/TemplatePanel.java
@@ -0,0 +1,36 @@
+package se.chalmers.tda367.group25.resumate.views;
+
+import javax.swing.JPanel;
+
+public class TemplatePanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TemplatePanel() {
+
+	}
+
+	/**
+	 * Returns the text in the personalInfo JEditorPane.
+	 * 
+	 * @return String in the JEditorPane for personal info
+	 */
+	public String getPersonalInfo() {
+		return null;
+	}
+
+	/**
+	 * Returns the text in the workingExperience JEditorPane.
+	 * 
+	 * @return String in the JEditorPane for working experience
+	 */
+	public String getWorkingExperienceText() {
+		return null;
+	}
+
+	public String getOtherText() {
+		return null;
+	}
+
+}

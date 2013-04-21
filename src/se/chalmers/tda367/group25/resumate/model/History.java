57dc4d38a1756ab37ff1abe07157ce014e797021
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-04-21 14:28:46 +0200

    JavaDoc and conversation History.
    Remade comments to JavaDoc, contributed to conversation about handling
    of History.

--- src/se/chalmers/tda367/group25/resumate/model/History.java
+++ src/se/chalmers/tda367/group25/resumate/model/History.java
@@ -1,31 +1,38 @@
 package se.chalmers.tda367.group25.resumate.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class History {
-	//The list could have versions of the document
-	//stored. Ideas not finished.
-	private List<Document> versions;
-	
-	
-	/*
-	 * Adds a version to the list. 
-	 * 
-	 * There should be a check so that the list only 
-	 * contains "so many" versions i.e. it should be 
-	 * cleaned once in a while.
-	 */
-	public void addVersion(){
-		
-	}
-	
-	/*
-	 * Get the previous version of the document.
-	 * List should be updated accordingly ;)
-	 */
 
-	public Document getPrevious(){
+	// Holds previous versions of the Document to which a given instance of
+	// History belongs.
+	private List<Document> versions;
+
+	public History() {
+		versions = new ArrayList<Document>(3); // One example of how to limit
+												// the size of the List
+	}
+
+	/**
+	 * Adds a version to the list.
+	 * 
+	 * There should be a check so that the list only contains "so many" versions
+	 * i.e. it should be cleaned once in a while. Maybe an if Statement removing
+	 * the oldest version of a Document if the List is already full?
+	 */
+	public void addVersion() {
+
+	}
+
+	/**
+	 * Get the previous version of the document. List should be updated
+	 * accordingly.
+	 * 
+	 * @return the previous version of a Document
+	 */
+	public Document getPrevious() {
 		return null;
 	}
-	
+
 }

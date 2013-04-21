121974f79132ad4d84e58b025d5133c09f6daf24
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-04-21 11:39:01 +0200

    Replaced docs list with controllers.
    DocController and IOController.

--- src/se/chalmers/tda367/group25/resumate/controllers/MainController.java
+++ src/se/chalmers/tda367/group25/resumate/controllers/MainController.java
@@ -8,24 +8,18 @@ import se.chalmers.tda367.group25.resumate.model.Template;
 
 public class MainController {
 
-	private List<Document> docs = new ArrayList<Document>();
+	private DocumentController documentController;
+	private IOController ioController;
 
 	/*An application with the default document is created.*/
 	public MainController(){
-		createDocument(Template.DEF_CV);
+		documentController = new DocumentController();
+		ioController = new IOController();
 	}
 
 	/*Creates a new document with the specified template*/
 	public void createDocument(Template templ){
 		Document doc = new Document(templ);
-		docs.add(doc);
-
-	}
-
-	/*Get the document of index i*/
-	public Document getDoc(int i){
-		return docs.get(i);
-
 	}
 	
 	/*Quit. Documents should be saved*/

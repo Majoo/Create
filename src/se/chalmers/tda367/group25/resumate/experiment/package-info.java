1b69116e11fd640629aec395d0f7468037867acf
Danny Lam <dannylam@live.se>
2013-04-23 10:58:52 +0200

    Experiment package.

---
+++ src/se/chalmers/tda367/group25/resumate/experiment/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 23/4-2013
+ */
+/**
+ * A package with classes for experiment
+ * Name each class with your name first and then what it does, 
+ * for example DannySaveDoc, PatriciaImageHandler
+ */
+package se.chalmers.tda367.group25.resumate.experiment;

0478038f7908569c27c70c9fbcbc87749fc4e55d
Patricia <chorriballong@gmail.com>
2013-04-24 11:37:20 +0200

    A first try in the methods of RMImage

--- src/se/chalmers/tda367/group25/resumate/model/RMImage.java
+++ src/se/chalmers/tda367/group25/resumate/model/RMImage.java
@@ -1,27 +1,60 @@
 package se.chalmers.tda367.group25.resumate.model;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
 public class RMImage {
 
-	private Image image;
+	private BufferedImage img;
 	private SectionType secType;
 
-	public RMImage(Image image) {
+	//-----Constructors-----//
+	/**
+	 * Creates an RMImage with a BufferedImage.
+	 * 
+	 * @param image
+	 *				the image to make an RMImage of.
+	 */
+	public RMImage(BufferedImage image) {
 		this.secType = SectionType.IMAGE;
-		this.image = image;
+		this.img = image;
 	}
 
+	// -----Queries-----//
+	/**
+	 * Get the BufferedImage.
+	 * 
+	 * @return image
+	 */
+	public BufferedImage getImage() {
+		return img;
+	}
+
+	// -----Commands-----//
+	/**
+	 * Set a new image.
+	 * 
+	 * @param image
+	 */
+	public void setImage(BufferedImage image) {
+		this.img = image;
+	}
+	
 	/**
 	 * Crop the Image of this RMImage.
+	 * (NEEDS TO BE TESTED)
 	 * 
-	 * @param x
-	 *            pixels to crop horizontally
-	 * @param y
-	 *            pixels to crop vertically
+	 * @param rect 
+	 * 				a rectangle with the coordinates
+	 * 				the image will have after the cropping.
 	 */
-	public void cropImage(int x, int y) {
-
+	public void crop(Rectangle rect) {
+		BufferedImage tmp = this.img.getSubimage(rect.x
+				,rect.y, rect.width, rect.height);
+		this.img = tmp;
+		
 	}
 
 	/**
@@ -32,39 +65,32 @@ public class RMImage {
 	 * @param y
 	 *            percentage to resize vertically
 	 */
-	public void resizeImage(int x, int y) {
+	public void resize(int x, int y) {
 
 	}
 
 	/**
 	 * Reorient the Image of this RMImage. A positive parameter rotates the
-	 * image counterclockwise, and vice versa.
+	 * image clockwise, and vice versa.
 	 * 
 	 * @param v
 	 *            degrees to rotate
 	 */
-	public void reorientImage(int v) {
+	public void reorient(int v) {
 
 	}
-
-	// -----Getters-----//
+	
 	/**
-	 * Get the Image.
-	 * 
-	 * @return image
+	 * Grayscale the Image of this RMImage.
+	 * (NEEDS TO BE TESTED)
 	 */
-	public Image getImage() {
-		return image;
-	}
-
-	// -----Setters-----//
-	/**
-	 * Set a new image.
-	 * 
-	 * @param image
-	 */
-	public void setImage(Image image) {
-		this.image = image;
+	public void makeGray(){
+		
+		BufferedImage image = new BufferedImage(this.img.getWidth(), this.img.getHeight(),  
+			    BufferedImage.TYPE_BYTE_GRAY);  
+			Graphics g = this.img.getGraphics();  
+			g.drawImage(this.img, 0, 0, null);  
+			g.dispose();
 	}
 
 }

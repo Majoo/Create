0b351686f352a8b314d0e16e3bc54166d87e74d9
Patricia <chorriballong@gmail.com>
2013-05-02 21:02:50 +0200

    began ToolbarPanel

--- src/se/chalmers/tda367/group25/resumate/views/ToolbarPanel.java
+++ src/se/chalmers/tda367/group25/resumate/views/ToolbarPanel.java
@@ -1,6 +1,8 @@
 package se.chalmers.tda367.group25.resumate.views;
 
 import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
 
 public class ToolbarPanel extends JPanel {
 
@@ -8,6 +10,13 @@ public class ToolbarPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public ToolbarPanel() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel upperPanel = new JPanel();
+		add(upperPanel, BorderLayout.NORTH);
+		
+		JPanel lowerPanel = new JPanel();
+		add(lowerPanel, BorderLayout.SOUTH);
 
 	}
 

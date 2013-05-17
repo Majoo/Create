e8f4b420b46bb16e22453eb318d783e220fc4b30
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-05-17 16:45:33 +0200

    Prepped IOExp branch

--- src/se/chalmers/tda367/group25/resumate/experimentText/Styles.java
+++
@@ -1,8 +0,0 @@
-package se.chalmers.tda367.group25.resumate.experimentText;
-
-public class Styles {
-
-	public static boolean B = false;
-	public static boolean U = false;
-	public static boolean I = false;
-}

34996db30feac9e9cae0c8b1f9b08809910d95eb
Laszlo Sall Vesselenyi <metaomega@gmail.com>
2013-05-18 18:12:32 +0200

    Improved code in PDFHandler; what's up in TemplatePanel?

--- src/se/chalmers/tda367/group25/resumate/model/PDFHandler.java
+++ src/se/chalmers/tda367/group25/resumate/model/PDFHandler.java
@@ -46,23 +46,18 @@ public class PDFHandler {
 
 		document.open();
 		PdfContentByte cb = writer.getDirectContent();
-		PdfTemplate tp = cb.createTemplate(panelWidth, panelHeight);
-		Graphics2D g2 = tp.createGraphicsShapes(panelWidth, panelHeight);
-		jc.print(g2);
-		g2.dispose();
-		cb.addTemplate(tp, (document.left()), (document.top() - panelHeight));
 
 		// If the incoming JComponent representation of a Document is larger
 		// than a single PDF document, create new pages accordingly
-		int delta = (int) (panelHeight - document.top());
+		int delta = panelHeight;
 		while (delta >= 0) {
 			document.newPage();
-			PdfTemplate tp2 = cb.createTemplate(panelWidth, panelHeight);
-			Graphics2D g22 = tp2.createGraphicsShapes(panelWidth, panelHeight);
-			jc.print(g22);
-			cb.addTemplate(tp2, document.left(document.leftMargin()),
+			PdfTemplate tp = cb.createTemplate(panelWidth, panelHeight);
+			Graphics2D g2 = tp.createGraphicsShapes(panelWidth, panelHeight);
+			jc.print(g2);
+			cb.addTemplate(tp, document.left(document.leftMargin()),
 					document.top() - delta);
-			g22.dispose();
+			g2.dispose();
 			delta = (int) (delta - document.top());
 		}
 

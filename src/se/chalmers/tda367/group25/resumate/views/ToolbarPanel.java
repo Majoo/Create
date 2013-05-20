a64d1ac798c185c7dfdf983e0ef6027e5a349d23
Danny Lam <dannylam@live.se>
2013-05-20 20:13:32 +0200

    Refactorated code in Toolbarpanel

--- src/se/chalmers/tda367/group25/resumate/views/ToolbarPanel.java
+++ src/se/chalmers/tda367/group25/resumate/views/ToolbarPanel.java
@@ -4,18 +4,10 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
-import se.chalmers.tda367.group25.resumate.controllers.MainController;
-
+import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.font.TextAttribute;
-
-import java.awt.BorderLayout;
-import java.awt.Toolkit;
-
-import javax.swing.SpringLayout;
-import java.awt.FlowLayout;
-import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.HashMap;
@@ -28,39 +20,16 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 import javax.swing.JToggleButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.SpringLayout;
-
-import se.chalmers.tda367.group25.resumate.model.Template;
-import se.chalmers.tda367.group25.resumate.utils.Labels;
-import se.chalmers.tda367.group25.resumate.views.MainView;
-
-import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
-
-import java.awt.Font;
-import java.awt.font.TextAttribute;
-import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.border.LineBorder;
-import java.awt.SystemColor;
+import se.chalmers.tda367.group25.resumate.utils.Labels;
 
-
-public class ToolbarPanel extends JPanel{
+public class ToolbarPanel extends JPanel implements ActionListener{
 	private PropertyChangeSupport pcs;
-
-
-	private String currentFileDirectory = "";
-	private JButton temp1But, temp2But, temp3But;
-	private JComboBox otherTemps;
 	private JComboBox textSizeCB;
 	private JComboBox fontCB;
-	private JToggleButton tglbtnBold;
-	private JToggleButton tglbtnUnderline;
-	private JToggleButton tglbtnItalic;
-	private JPanel lowerToolsPan;
 
 	/**
 	 * Create the panel.
@@ -71,131 +40,75 @@ public class ToolbarPanel extends JPanel{
 		SpringLayout spLayout = new SpringLayout();
 		setLayout(spLayout);
 
-		//Tools panel
+		//Setting Tools panel
 		JPanel toolsPan = new JPanel();
 		spLayout.putConstraint(SpringLayout.NORTH, toolsPan, 0, SpringLayout.NORTH, this);
 		spLayout.putConstraint(SpringLayout.WEST, toolsPan, 0, SpringLayout.WEST, this);
 		spLayout.putConstraint(SpringLayout.SOUTH, toolsPan, 69, SpringLayout.NORTH, this);
-
 		toolsPan.setLayout(new GridLayout(2,1));
-		//Upper part tools panel
+		
+		//Setting upper part tools panel
 		JPanel upperToolsPan = new JPanel();
 		upperToolsPan.setBackground(Color.WHITE);
 		upperToolsPan.setLayout(new GridLayout(1,10));
 		toolsPan.add(upperToolsPan);
 
+		//Setting all the buttons and giving them actions
 		JButton btnNewDoc = new JButton("New");
-		btnNewDoc.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				int selection = JOptionPane.showConfirmDialog(null,
-						"Do you want to save the document first?", null,
-						JOptionPane.YES_NO_OPTION);
-				if (selection == JOptionPane.YES_OPTION) {
-					JOptionPane.showMessageDialog(null, "Open an explorer to save an .rsmt-file HERE"); //implementera Save här
-					//Toolkit.getDefaultToolkit().beep();
-					//tabbedpane.addTab("Tab 2", null, docView, "unsaved");
-				}else if(selection == JOptionPane.CLOSED_OPTION){
-					;
-				}else{
-					 
-					//tabbedpane.addTab("Tab 2", null, docView, "unsaved");
-					
-				}
-			}
-		});
+		btnNewDoc.addActionListener(this);
+		btnNewDoc.setActionCommand("New");
 		upperToolsPan.add(btnNewDoc);
 
 		JButton btnOpen = new JButton("Open");
-		btnOpen.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				JOptionPane.showMessageDialog(null, "this should open a doc");
-			}
-		});
+		btnOpen.addActionListener(this);
+		btnOpen.setActionCommand("Open");
 		upperToolsPan.add(btnOpen);
 
 		JButton btnSave = new JButton("Save");
-		/*btnSave.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				if("".equals(currentFileDirectory)){					//if the current file is a new one (untitled)
-					JFileChooser sdChooser = new JFileChooser();		//file chooser
-					FileNameExtensionFilter filter = new FileNameExtensionFilter("Resumate File", "rsmt");
-					sdChooser.setFileFilter(filter);
-					int returnVal = sdChooser.showSaveDialog(null);
-					
-					try{
-						if(returnVal == JFileChooser.APPROVE_OPTION){
-							File directory = sdChooser.getCurrentDirectory();
-							String path = directory.getAbsolutePath();					//the absolute path of the directory, named "path"
-							String fileName = sdChooser.getSelectedFile().getName();	//get the file name
-							if(!fileName.contains("rsmt")){								//if the file name doesn't contain rsmt,
-								fileName = fileName + ".rsmt";							//name it a new name with .rsmt at the end
-							}
-							BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path + "\\" + fileName), "UTF-8"));
-							currentFileDirectory = path + "\\" + fileName;				//the current file directory is now "theabsolutepath\\filename.rsmt"
-							//bw.write(THE_NAME_OF_THE_EDITORPANE_THAT_SHOULD_BE_OVERWRITTEN.getText());					//get the document text and write it over
-							bw.close();
-						}
-						
-					}catch(IOException err){
-						JOptionPane.showMessageDialog(null,  "ERROR!");
-					}
-				
-				}else{
-					
-					try{
-						//if it is not empty, we'll save it into the current directory
-						BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(currentFileDirectory), "UTF-8"));
-						//bw.write(THE_NAME_OF_THE_EDITORPANE_THAT_SHOULD_BE_OVERWRITTEN.getText());
-						bw.close();
-						
-					}catch(IOException err){
-						JOptionPane.showMessageDialog(null,  "ERROR!");
-					}	
-				}
-			}
-				
-		});*/
+		btnSave.addActionListener(this);
+		btnSave.setActionCommand("Save");
 		upperToolsPan.add(btnSave);
 
 		JButton btnSend = new JButton("Send");
+		btnSend.setActionCommand("Send");
 		upperToolsPan.add(btnSend);
 
 		JButton btnPrint = new JButton("Print");
+		btnPrint.setActionCommand("Print");
 		upperToolsPan.add(btnPrint);
 
 		JButton btnCut = new JButton("Cut");
+		btnPrint.setActionCommand("Cut");
 		upperToolsPan.add(btnCut);
 
 		JButton btnCopy = new JButton("Copy");
-		btnCopy.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				pcs.firePropertyChange(Labels.TEXT_COPY, false, true);
-			}
-		});
+		btnCopy.addActionListener(this);
+		btnCopy.setActionCommand("Copy");
 		upperToolsPan.add(btnCopy);
 
 		JButton btnPaste = new JButton("Paste");
+		btnPaste.addActionListener(this);
+		btnPaste.setActionCommand("Paste");
 		upperToolsPan.add(btnPaste);
 
-		JButton btnForward = new JButton("Forward");
-		upperToolsPan.add(btnForward);
+		JButton btnRedo = new JButton("Redo");
+		btnRedo.addActionListener(this);
+		btnRedo.setActionCommand("Redo");
+		upperToolsPan.add(btnRedo);
 
-		JButton btnBackward = new JButton("Backward");
-		upperToolsPan.add(btnBackward);
+		JButton btnUndo = new JButton("Undo");
+		btnUndo.addActionListener(this);
+		btnUndo.setActionCommand("Undo");
+		upperToolsPan.add(btnUndo);
 		
 		//Lower part tools panel
-		lowerToolsPan = new JPanel();
+		JPanel lowerToolsPan = new JPanel();
 		lowerToolsPan.setBackground(Color.WHITE);
 
 		// Setting properties for the combobox in which the fonts are listed
 		fontCB = new JComboBox();
-		fontCB.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				pcs.firePropertyChange(Labels.TEXTFONT_CHANGED, fontCB, fontCB.getSelectedItem().toString());
-			}
-		});
-
+		fontCB.addActionListener(this);
+		fontCB.setActionCommand("Font");
 		GraphicsEnvironment e = GraphicsEnvironment
 				.getLocalGraphicsEnvironment();
 		String [] fontes = e.getAvailableFontFamilyNames();
@@ -203,40 +116,29 @@ public class ToolbarPanel extends JPanel{
 
 		// Setting properties for the combobox in which the sizes for the text are listed
 		textSizeCB = new JComboBox();
-		textSizeCB.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				pcs.firePropertyChange(Labels.TEXTSIZE_CHANGED, textSizeCB, textSizeCB.getSelectedItem().toString());
-			}
-		});
+		textSizeCB.addActionListener(this);
+		textSizeCB.setActionCommand("Size");
+		
 		textSizeCB.setModel(new DefaultComboBoxModel(new String[] {"7", "8", "9", "10"
 				, "11", "12", "13", "14", "15", "16", "18", "20", "22", "24", "26", "28"
 				, "30", "32", "36", "40", "44", "48", "52", "56", "60"}));
 
 		// Setting properties for the button which makes the text bold
-		tglbtnBold = new JToggleButton("B");
-		tglbtnBold.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				pcs.firePropertyChange(Labels.TEXTSTYLE_CHANGED, tglbtnBold, "B");
-			}
-		});
+		JToggleButton tglbtnBold = new JToggleButton("B");
+		tglbtnBold.addActionListener(this);
+		tglbtnBold.setActionCommand("Bold");
 		tglbtnBold.setFont(new Font("Tahoma", Font.BOLD, 11));
 
 		// Setting properties for the button which make the text italic
-		tglbtnItalic = new JToggleButton("I");
-		tglbtnItalic.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				pcs.firePropertyChange(Labels.TEXTSTYLE_CHANGED, tglbtnItalic, "I");
-			}
-		});
+		JToggleButton tglbtnItalic = new JToggleButton("I");
+		tglbtnItalic.addActionListener(this);
+		tglbtnItalic.setActionCommand("Italic");	
 		tglbtnItalic.setFont(new Font("Tahoma", Font.ITALIC, 11));
 
 		// Setting properties for the button which make the text underlined
-		tglbtnUnderline = new JToggleButton("U");
-		tglbtnUnderline.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				pcs.firePropertyChange(Labels.TEXTSTYLE_CHANGED, tglbtnUnderline, "U");
-			}
-		});
+		JToggleButton tglbtnUnderline = new JToggleButton("U");
+		tglbtnUnderline.addActionListener(this);
+		tglbtnUnderline.setActionCommand("Underline");	
 		Map<TextAttribute, Object> map = new HashMap<TextAttribute, Object>();
 		map.put(TextAttribute.UNDERLINE,
 				TextAttribute.UNDERLINE_ON);
@@ -247,6 +149,7 @@ public class ToolbarPanel extends JPanel{
 		textColorCB.setModel(new DefaultComboBoxModel(new String[] {"color"}));
 		textColorCB.setForeground(Color.RED);
 
+		//Setting placement of the GUI
 		GroupLayout gl_lowerToolsPan = new GroupLayout(lowerToolsPan);
 		gl_lowerToolsPan.setHorizontalGroup(
 			gl_lowerToolsPan.createParallelGroup(Alignment.LEADING)
@@ -279,10 +182,7 @@ public class ToolbarPanel extends JPanel{
 		);
 		lowerToolsPan.setLayout(gl_lowerToolsPan);
 		toolsPan.add(lowerToolsPan);
-
-
 		add(toolsPan);
-
 
 		//Template chooser panel
 		JPanel tempsPan = new JPanel();
@@ -293,27 +193,23 @@ public class ToolbarPanel extends JPanel{
 		spLayout.putConstraint(SpringLayout.SOUTH, tempsPan, 69, SpringLayout.NORTH, this);
 		spLayout.putConstraint(SpringLayout.EAST, tempsPan, -10, SpringLayout.EAST, this);
 
+		//Setting template buttons
 		tempsPan.setLayout(new GridLayout(1, 4));
-		temp1But = new JButton("Default CL");
-		temp1But.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//pcs.firePropertyChange(Labels.TEMPLATE_CHANGED, null, Template.DEF_PL);
-			}
-		});
+		JButton temp1But = new JButton("Default PL");
+		temp1But.addActionListener(this);
+		temp1But.setActionCommand("DefPL");
 		tempsPan.add(temp1But);
-		temp2But = new JButton("temp2");
+		
+		JButton temp2But = new JButton("temp2");
 		tempsPan.add(temp2But);
-		temp3But = new JButton("temp3");
+		JButton temp3But = new JButton("temp3");
 		tempsPan.add(temp3But);
-		otherTemps = new JComboBox();
+		JComboBox otherTemps = new JComboBox();
 		tempsPan.add(otherTemps);
-
 		add(tempsPan);
 
 		//PropertyChangeSupport, Listeners and other important stuff.
 		pcs = new PropertyChangeSupport(this);
-
-
 	}
 
 	/**
@@ -328,5 +224,101 @@ public class ToolbarPanel extends JPanel{
 	 */
 	public void removePropertyChangeListener(PropertyChangeListener pcl){
 		pcs.removePropertyChangeListener(pcl);
+	}
+	/**
+	 * Settles the actions to be performed depeding on 
+	 * which component which was the source.
+	 */
+	public void actionPerformed(ActionEvent arg0) {
+		
+		switch(arg0.getActionCommand()){
+		case "Copy":
+			pcs.firePropertyChange(Labels.TEXT_COPY, false, true);
+			break;
+		case "Paste":
+			pcs.firePropertyChange(Labels.TEXT_PASTE, false, true);
+			break;
+		case "Undo":
+			pcs.firePropertyChange(Labels.UNDO_ACTION, false, true);
+			break;
+		case "Redo":	
+			pcs.firePropertyChange(Labels.REDO_ACTION, false, true);
+			break;
+		case "Font":
+			pcs.firePropertyChange(Labels.TEXTFONT_CHANGED, null, fontCB.getSelectedItem().toString());
+			break;
+		case "Size":	
+			pcs.firePropertyChange(Labels.TEXTSIZE_CHANGED, null, textSizeCB.getSelectedItem().toString());
+			break;
+		case "Bold":
+			pcs.firePropertyChange(Labels.TEXTSTYLE_CHANGED, null, "B");
+			break;
+		case "Italic":	
+			pcs.firePropertyChange(Labels.TEXTSTYLE_CHANGED, null, "I");
+			break;
+		case "Underline":
+			pcs.firePropertyChange(Labels.TEXTSTYLE_CHANGED, null, "U");
+			break;
+		case "Open":
+			JOptionPane.showMessageDialog(null, "this should open a doc");
+			break;
+		case "New":
+		/*	int selection = JOptionPane.showConfirmDialog(null,
+					"Do you want to save the document first?", null,
+					JOptionPane.YES_NO_OPTION);
+			if (selection == JOptionPane.YES_OPTION) {
+				JOptionPane.showMessageDialog(null, "Open an explorer to save an .rsmt-file HERE"); //implementera Save här
+				//Toolkit.getDefaultToolkit().beep();
+				//tabbedpane.addTab("Tab 2", null, docView, "unsaved");
+			}else if(selection == JOptionPane.CLOSED_OPTION){
+				//Do nothing
+			}else{
+				//tabbedpane.addTab("Tab 2", null, docView, "unsaved");
+			}*/
+		break;
+		case "Save":
+			/* String currentFileDirectory = "";
+			if("".equals(currentFileDirectory)){					//if the current file is a new one (untitled)
+				JFileChooser sdChooser = new JFileChooser();		//file chooser
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("Resumate File", "rsmt");
+				sdChooser.setFileFilter(filter);
+				int returnVal = sdChooser.showSaveDialog(null);
+				
+				try{
+					if(returnVal == JFileChooser.APPROVE_OPTION){
+						File directory = sdChooser.getCurrentDirectory();
+						String path = directory.getAbsolutePath();					//the absolute path of the directory, named "path"
+						String fileName = sdChooser.getSelectedFile().getName();	//get the file name
+						if(!fileName.contains("rsmt")){								//if the file name doesn't contain rsmt,
+							fileName = fileName + ".rsmt";							//name it a new name with .rsmt at the end
+						}
+						BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path + "\\" + fileName), "UTF-8"));
+						currentFileDirectory = path + "\\" + fileName;				//the current file directory is now "theabsolutepath\\filename.rsmt"
+						//bw.write(THE_NAME_OF_THE_EDITORPANE_THAT_SHOULD_BE_OVERWRITTEN.getText());					//get the document text and write it over
+						bw.close();
+					}
+					
+				}catch(IOException err){
+					JOptionPane.showMessageDialog(null,  "ERROR!");
+				}
+			
+			}else{
+				
+				try{
+					//if it is not empty, we'll save it into the current directory
+					BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(currentFileDirectory), "UTF-8"));
+					//bw.write(THE_NAME_OF_THE_EDITORPANE_THAT_SHOULD_BE_OVERWRITTEN.getText());
+					bw.close();
+					
+				}catch(IOException err){
+					JOptionPane.showMessageDialog(null,  "ERROR!");
+				}	
+			}*/
+			break;
+		case "DefPL":
+			//pcs.firePropertyChange(Labels.TEMPLATE_CHANGED, null, Template.DEF_PL);
+			break;
+		default: //do nothing, never invoked.	
+		}
 	}
 }
